46e980bf3270c8b47eb17fff6a983fb8e0828481
koala <3237584377@qq.com>
2016-02-27 10:55:29 +0800

    Foodie

---
+++ Foodie/DaoJia/SDK/Topic/View/ScrollerCell.h
@@ -0,0 +1,13 @@
+//
+//  ScrollerCell.h
+//  好豆菜谱ZN
+//
+//  Created by Macx on 16/2/19.
+//  Copyright © 2016年 Macx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollerCell : UITableViewCell
+
+@end
---
+++ Foodie/DaoJia/SDK/Topic/View/ScrollerCell.m
@@ -0,0 +1,23 @@
+//
+//  ScrollerCell.m
+//  好豆菜谱ZN
+//
+//  Created by Macx on 16/2/19.
+//  Copyright © 2016年 Macx. All rights reserved.
+//
+
+#import "ScrollerCell.h"
+
+@implementation ScrollerCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
